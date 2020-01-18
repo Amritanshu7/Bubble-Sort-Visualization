696f32a46c0e9601726467b47f7bb61e4fa575a3
Amritanshu7 <50537986+Amritanshu7@users.noreply.github.com>
2020-01-18 23:27:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let values = [];
 let i = 0, j = 0, w = 5;
 
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(1366 , 768);
   colorMode(HSB, height);
   
   values = new Array(floor(width / w));
